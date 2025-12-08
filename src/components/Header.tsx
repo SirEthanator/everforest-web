7e986a81a19373df3465410c0a7cc3147f781319
Ethan <SirEthanator@users.noreply.github.com>
2025-12-08 20:24:18 +1300

    Header: Add fancy background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,10 @@ export type HeaderProps = {
 export default function Header({ children }: HeaderProps) {
   return (
     <div className={s.header}>
-      <div className={s.content}>
-        <h1 className={s.title}>{children}</h1>
+      <div className={s.headerBg}>
+        <div className={s.content}>
+          <h1 className={s.title}>{children}</h1>
+        </div>
       </div>
     </div>
   );
