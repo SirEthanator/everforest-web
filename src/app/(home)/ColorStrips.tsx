672bd1312a4e7449196154a8bb96b0ff0f9a9fc8
Ethan <SirEthanator@users.noreply.github.com>
2025-12-13 15:48:59 +1300

    Home/Color Strips: Improve logic and style and add comments

--- src/app/(home)/ColorStrips.tsx
+++ src/app/(home)/ColorStrips.tsx
@@ -3,7 +3,7 @@
 import { useCallback, useEffect, useState } from "react";
 import s from "./styles/ColorStrips.module.scss";
 
-const SLIDE_LENGTH = 900;
+const SLIDE_LENGTH = 950;
 const SLIDE_DELAY = 2500;
 
 const COLORS = [
@@ -17,11 +17,12 @@ const COLORS = [
 
 type Color = (typeof COLORS)[number];
 
-type StripProps = {
+type SlidingStripProps = {
   idx: number;
+  tick: number;
 };
 
-function Strip({ idx }: StripProps) {
+function SlidingStrip({ idx, tick }: SlidingStripProps) {
   const direction = idx % 2 === 0 ? "up" : "down";
   const [currentColorIdx, setCurrentColorIdx] = useState(idx);
   const [activeStrip, setActiveStrip] = useState<0 | 1>(0);
@@ -29,46 +30,62 @@ function Strip({ idx }: StripProps) {
 
   const rotateStrips = useCallback(() => {
     setActiveStrip((prev) => (prev === 1 ? 0 : 1));
+    setCurrentColorIdx((prev) => prev + 1);
     setIsAnimating(true);
 
     return setTimeout(() => {
       setIsAnimating(false);
-      setCurrentColorIdx((prev) => prev + 1);
     }, SLIDE_LENGTH);
   }, []);
 
   useEffect(() => {
-    let timeout = rotateStrips();
+    // tick is not needed for any logic, it is to control
+    // when this effect should fire in order to synchronise
+    // all the strips rotating.
+    void tick;
 
-    const interval = setInterval(() => {
-      timeout = rotateStrips();
-    }, SLIDE_DELAY + SLIDE_LENGTH);
+    const timeout = rotateStrips();
 
-    return () => {
-      clearInterval(interval);
-      clearTimeout(timeout);
-    };
-  }, [rotateStrips]);
+    return () => clearTimeout(timeout);
+  }, [tick, rotateStrips]);
 
   return (
     <div className={s.strip}>
       {[0, 1].map((stripIdx: number) => {
+        // If this is the active strip position is 0
+        // It is also 0 if the animation is in progress so
+        // that it is still visible before moving up/down
+        // to where it will start from (the bottom if direction
+        // is up or the top if direction is down).
         const yTranslation =
           (activeStrip === stripIdx || isAnimating
             ? 0
             : direction === "up"
               ? 100
               : -100) -
+          // Subtract 100 from second strip since its base pos is greater by 100%
           stripIdx * 100;
+
+        // If this is the active strip, use the current color, otherwise
+        // use the previous one so it is still showing during the animation.
+        const bg =
+          COLORS[
+            (currentColorIdx - (activeStrip === stripIdx ? 0 : 1)) %
+              COLORS.length
+          ];
 
         return (
           <div
             key={stripIdx}
             className={s.stripInner}
             style={{
-              backgroundColor: `var(--${COLORS[activeStrip !== stripIdx || !isAnimating ? currentColorIdx % COLORS.length : (currentColorIdx + 1) % COLORS.length]})`,
+              backgroundColor: `var(--${bg})`,
               transform: `translateY(${yTranslation}%)`,
-              transition: `transform ${SLIDE_LENGTH}ms ease-in-out`,
+              // Ease in out expo
+              transition: `transform ${SLIDE_LENGTH}ms cubic-bezier(0.87, 0, 0.13, 1)`,
+              // The active strip should always be on top. This can create
+              // the appearance of it pushing the old strip out when really
+              // it's just sliding over it.
               zIndex: activeStrip === stripIdx ? "1" : "0"
             }}
           />
@@ -79,10 +96,25 @@ function Strip({ idx }: StripProps) {
 }
 
 export default function ColorStrips() {
+  // To synchronise effects of the strips
+  const [tick, setTick] = useState(0);
+
+  useEffect(() => {
+    // Each strip's effect will fire when tick changes.
+    const interval = setInterval(() => {
+      setTick((prev) => prev + 1);
+
+      // SLIDE_LENGTH is added because each strip's effect
+      // contains a timeout of delay SLIDE_LENGTH.
+    }, SLIDE_DELAY + SLIDE_LENGTH);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={s.root}>
       {COLORS.map((startColor: Color, idx: number) => (
-        <Strip idx={idx} key={startColor} />
+        <SlidingStrip idx={idx} tick={tick} key={startColor} />
       ))}
     </div>
   );
