900b1f8dc2e2cb9a75b3118cbc8059a2063427be
Ethan <SirEthanator@users.noreply.github.com>
2025-12-19 18:07:25 +1300

    Footer: Add github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,13 @@
+import { IconBrandGithub } from "@tabler/icons-react";
 import Link from "next/link";
 import s from "./styles/Footer.module.scss";
+
+const GITHUB_URL = "https://github.com/SirEthanator/everforest-web";
 
 export default function Footer() {
   return (
     <div className={s.root}>
-      <p className={s.text}>
+      <p>
         © Copyright 2025 Ethan M. Website design and development by Ethan M.
         Color scheme "Everforest" created by{" "}
         <Link href="https://github.com/sainnhe" target="_blank">
@@ -12,6 +15,10 @@ export default function Footer() {
         </Link>
         . All rights reserved.
       </p>
+
+      <Link className={s.githubLink} href={GITHUB_URL} target="_blank">
+        <IconBrandGithub />
+      </Link>
     </div>
   );
 }
